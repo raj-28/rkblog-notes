63367667bad98efc9891f6d6f2f907fbadea2493
pramit-marattha <pramitmarattha5497@gmail.com>
2022-12-27 09:42:28 +0545

    Remove tweets menu item

--- components/DropMenu.js
+++ components/DropMenu.js
@@ -187,24 +187,6 @@ export default function DropMenu() {
                 </Link>
               )}
             </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <Link href="/tweets">
-                  <a
-                    className={classNames(
-                      active
-                        ? 'bg-gray-200 text-gray-700 dark:bg-zinc-700 dark:text-gray-300'
-                        : 'bg-white text-zinc-700 hover:bg-gray-300 dark:bg-zinc-800 dark:text-gray-200 dark:hover:bg-zinc-700',
-                      'block px-4 py-2 text-sm'
-                    )}
-                  >
-                    <div className="flex flex-row">
-                      <TwitterLogoIcon className="mr-4 mt-0.5" /> Tweets
-                    </div>
-                  </a>
-                </Link>
-              )}
-            </Menu.Item>
           </div>
         </Menu.Items>
       </Transition>
